fa15c489eb69d677b06db4b39db4972f80c67d15
blackholeinfiverse145 <blackholeinfiverse145@gmail.com>
2026-07-09 15:10:45 +0530

    Created dashboard entry page

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,14 @@
+export default function Dashboard() {
+  return (
+    <div
+      style={{
+        background: "red",
+        color: "white",
+        padding: "40px",
+        fontSize: "40px",
+      }}
+    >
+      Dashboard Working
+    </div>
+  );
+}
